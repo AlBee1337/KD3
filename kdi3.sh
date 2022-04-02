251ea943643fbed3653adf33f1d19cb81f8205fc
albee <haxr.albee@gmail.com>
2022-04-03 00:13:11 +0200

    add config and modified script

--- kdi3.sh
+++ kdi3.sh
@@ -1,2 +1,11 @@
 #! /usr/bin/env bash
 
+git clone https://github.com/kwin-scripts/kwin-tiling.git
+cd kwin-tiling/
+plasmapkg2 --type kwinscript -i .
+kcmshell5 kwinscripts
+cd ..
+
+cp ./configs/krunnerrc $HOME/.config/krunnerrc
+cp ./configs/kglobalshortcutsrc $HOME/.config/kglobalshortcutsrc
+cp ./configs/plasma-org.kde.plasma.desktop-appletsrc $HOME/./config/plasma-org.kde.plasma.desktop-appletsrc
