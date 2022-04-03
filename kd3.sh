88b9efb23a881a76de9c26c64f859ce24c85aa72
albee <haxr.albee@gmail.com>
2022-04-03 19:22:34 +0200

    test condition

--- kd3.sh
+++ kd3.sh
@@ -1,31 +1,45 @@
 #! /usr/bin/env bash
 
-if [ -z $2 ] && [ ! -f $HOME/.config/*.kd3 ]
+if [ -z $2 ] 
 then
    case "$1" in
     -i|--install)
-        git clone https://github.com/kwin-scripts/kwin-tiling.git
-        cd kwin-tiling/
-        plasmapkg2 --type kwinscript -i .
-        cd ..
-        mv $HOME/.config/krunnerrc $HOME/.config/krunnerrc.kd3
-        mv $HOME/.config/kglobalshortcutsrc $HOME/.config/kglobalshortcutsrc.kd3
-        mv $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc.kd3
-        mv $HOME/.config/kwinrc $HOME/.config/kwinrc.kd3
-        cp ./configs/krunnerrc $HOME/.config/krunnerrc
-        cp ./configs/kglobalshortcutsrc $HOME/.config/kglobalshortcutsrc
-        cp ./configs/plasma-org.kde.plasma.desktop-appletsrc $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
-        cp ./configs/kwinrc $HOME/.config/kwinrc
-        rm -fr kwin-tiling
-        echo "\ncreated backup in $HOME/.config "
-        echo "krunnerrc.kd3"
-        echo "kglobalshortcutsrc.kd3"
-        echo "plasma-org.kde.plasma.desktop-appletsrc.kd3"
-        echo "kwinrc.kd3"
-        echo "##################################"
-        echo "#### DONT REMOVE BACKUP FILES ####"
-        echo "##################################"
+        if [ -f $HOME/.config/*.kd3 ];then 
+            echo "###############################"
+            echo "#### KD3 already installed ####"
+            echo "###############################"  
+        else 
+            git clone https://github.com/kwin-scripts/kwin-tiling.git
+            cd kwin-tiling/
+            plasmapkg2 --type kwinscript -i .
+            cd ..
+            mv $HOME/.config/krunnerrc $HOME/.config/krunnerrc.kd3
+            mv $HOME/.config/kglobalshortcutsrc $HOME/.config/kglobalshortcutsrc.kd3
+            mv $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc.kd3
+            mv $HOME/.config/kwinrc $HOME/.config/kwinrc.kd3
+            cp ./configs/krunnerrc $HOME/.config/krunnerrc
+            cp ./configs/kglobalshortcutsrc $HOME/.config/kglobalshortcutsrc
+            cp ./configs/plasma-org.kde.plasma.desktop-appletsrc $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
+            cp ./configs/kwinrc $HOME/.config/kwinrc
+            rm -fr kwin-tiling
+            echo "\ncreated backup in $HOME/.config "
+            echo "krunnerrc.kd3"
+            echo "kglobalshortcutsrc.kd3"
+            echo "plasma-org.kde.plasma.desktop-appletsrc.kd3"
+            echo "kwinrc.kd3"
+            echo "##################################"
+            echo "#### DONT REMOVE BACKUP FILES ####"
+            echo "##################################"
+        fi 
       shift
+      ;;
+    -u|--uninstall)
+        mv $HOME/.config/krunnerrc.kd3 $HOME/.config/krunnerrc
+        mv $HOME/.config/kglobalshortcutsrc.kd3 $HOME/.config/kglobalshortcutsrc
+        mv $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc.kd3 $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
+        mv $HOME/.config/kwinrc.kd3 $HOME/.config/kwinrc
+        echo "restored settings before KD3 was installed"
+        shift 
       ;;
     -h|--help)
         echo "HELP:\n"
@@ -39,13 +53,6 @@ then
         exit 1
       ;;
   esac
-elif [ $1 == "-u" || $1 == "--uninstall" ]
-then
-    mv $HOME/.config/krunnerrc.kd3 $HOME/.config/krunnerrc
-    mv $HOME/.config/kglobalshortcutsrc.kd3 $HOME/.config/kglobalshortcutsrc
-    mv $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc.kd3 $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
-    mv $HOME/.config/kwinrc.kd3 $HOME/.config/kwinrc
-    echo "restored settings before KD3 was installed"
 else
     echo "\n###########################################################"
     echo "#### Error: To much arguments or KD3 already installed ####"
